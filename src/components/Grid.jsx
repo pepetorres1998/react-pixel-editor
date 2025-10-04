4b73de9729991dcc39ee3e6d3eb569dc2bff1e47
Jose Antonio Torres Garibay <pepe_torres1998@hotmail.com>
2025-10-03 21:22:38 -0600

    Add optimization for Pixel rendering

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,17 +1,36 @@
 import Pixel from './Pixel';
 import { useGrid } from '../contexts/GridContext.jsx';
 import { useToolbar } from '../contexts/ToolbarContext.jsx';
+import { useCallback, useEffect, useRef } from 'react';
 
 const Grid = ({ selectedColor }) => {
   const gridSize = 32;
   const { gridColors, setGridColors } = useGrid();
   const { isDrawing } = useToolbar();
+  const isDrawingRef = useRef(isDrawing);
 
-  const handlePixelUpdate = (index) => {
-    const newPixels = [...gridColors];
-    newPixels[index] = selectedColor;
-    setGridColors(newPixels);
-  };
+  useEffect(() => { isDrawingRef.current = isDrawing }, [isDrawing]);
+
+  const handlePixelUpdate = useCallback((index) => {
+    setGridColors(prev => {
+      const next = [...prev];
+      next[index] = selectedColor;
+      return next;
+    });
+  }, [selectedColor]);
+
+  const handleClick = useCallback((event) => {
+    const index = Number(event.currentTarget.dataset.index);
+
+    handlePixelUpdate(index);
+  }, [handlePixelUpdate]);
+
+  const handleMouseEnter = useCallback((event) => {
+    if(!isDrawingRef.current) return;
+    const index = Number(event.currentTarget.dataset.index);
+
+    handlePixelUpdate(index);
+  }, [handlePixelUpdate]);
 
   return (
     <div
@@ -24,8 +43,9 @@ const Grid = ({ selectedColor }) => {
         <Pixel
           key={index}
           color={color}
-          onClick={() => handlePixelUpdate(index)}
-          onMouseEnter={() => isDrawing && handlePixelUpdate(index)}
+          index={index}
+          onClick={handleClick}
+          onMouseEnter={handleMouseEnter}
         />
       ))}
     </div>
--- src/components/Pixel.jsx
+++ src/components/Pixel.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const Pixel = React.memo(({ color, onClick, onMouseEnter }) => {
+const Pixel = React.memo(({ color, index, onClick, onMouseEnter }) => {
+  console.log('Rendering pixel with color:', color);
   return (
     <div
       className={`w-4 h-4 border border-gray-300`}
       style={{ backgroundColor: color }}
+      data-index={index}
       onClick={onClick}
       onMouseEnter={onMouseEnter}
     ></div>
