7e7dbf94474a429ababd50331d544eca6a349fb8
Jose Antonio Torres Garibay <pepe_torres1998@hotmail.com>
2025-10-11 20:28:33 -0600

    Add base url to url encoding

--- src/lib/UrlEncoder.js
+++ src/lib/UrlEncoder.js
@@ -3,13 +3,18 @@ import { Token } from "./url_encoder/token";
 
 export class UrlEncoder {
   static encode(grid) {
+    console.log('Encoding');
     const token = new Token(grid, 32);
-    const url = `localhost:5173?g=${token.build()}`;
+    const url = new URL(window.location.pathname, window.location.origin);
 
-    return url;
+    url.search = "";
+    url.searchParams.set("g", token.build());
+
+    return url.toString();
   }
 
   static decode(token) {
+    console.log('Decoding');
     const decoder = new Decoder(token);
 
     return decoder.decode();
